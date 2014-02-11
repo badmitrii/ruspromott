04bf268b765164bf7bbe67ae01712cf07c405a55
B.Dmitrii <bundos90@gmail.com>
2014-02-11 11:05:14 +0400

    url modified

--- app/models/model_task2.php
+++ app/models/model_task2.php
@@ -5,7 +5,7 @@ class Model_task2 extends Model{
     public function get_data(){
         $ch= curl_init();
         curl_setopt($ch, CURLOPT_URL, 
-                "http://www.1c-bitrix.ru/");
+                "http://www.stackoverflow.com//");
         $output= curl_exec($ch);
         curl_close($ch);
         return $output;
