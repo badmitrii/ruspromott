0275ea339a85f3063f1b65599be4ec749c0b1527
B.Dmitrii <bundos90@gmail.com>
2014-02-11 22:24:12 +0400

    Table for task 3

---
+++ app/views/t3content_view.php
@@ -0,0 +1,45 @@
+<head>
+<style>
+    
+    td{
+        padding: 2px;
+        font-size: 0.6em;
+    }
+    </style>
+</head>
+<body>
+    
+<div>
+    <table style="border: 1px solid black; margin:auto; max-width: 90%;">
+        <tr>
+            <td>
+                ГЛАВНАЯ
+            </td>
+            <td>
+                О КОМПАНИИ
+            </td>
+            <td>
+               ЧАСТНЫМ ЛИЦАМ
+            </td>
+            <td>
+                ЮРИДИЧЕСКИ ЛИЦАМ
+            </td>
+            <td>
+                СЛУЖБА ПОДДЕРЖКИ
+            </td>
+            <td>
+                ИНФОРМАЦИЯ
+            </td>
+            <td>
+                СПРАВКА
+            </td>
+            <td>
+                КЛИЕНТЫ
+            </td>
+            <td>
+                КОНТАКТЫ
+            </td>
+        </tr>
+    </table>
+</div>
+</body>
