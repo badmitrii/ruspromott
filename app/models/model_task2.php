7b4cab49b35a6c8d721714ccbe1cdd0b2a5a176c
B.Dmitrii <bundos90@gmail.com>
2014-02-12 11:22:10 +0400

    Url task2 redirecting fixed

--- app/models/model_task2.php
+++ app/models/model_task2.php
@@ -4,11 +4,14 @@ class Model_task2 extends Model{
     
     public function get_data(){
         $ch= curl_init();
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_URL, 
-                "http://stackoverflow.com//");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                "http://1c-bitrix.ru/");
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
         $output= curl_exec($ch);
         curl_close($ch);
-        return $output;
+	return $output;
     }
 }
